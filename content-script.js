89b56c67b8ea362cb919a278b2f559ce10b3b6f8
OJII3 <kawaisounayuriko@gmail.com>
2022-05-18 10:11:58 +0900

    Fix countDisplay
    counter wasn't shown on the right spacd

--- content-script.js
+++ content-script.js
@@ -6,13 +6,14 @@ const countWords = () => {
       counts[i].innerHTML = ` ${textareas[i].value.replace(/\s/g, '').length}文字`;
     }
   } else {
+
     for (let i = 0; i < textareas.length; i++) {
-      const countDisplaySpaces = document.querySelectorAll('.geS5n .G4EHhc');
+      const countDisplay = textareas[i].closest('.geS5n').querySelector('.G4EHhc');
       const count = document.createElement('span');
       count.className = 'word-counter';
       count.style.color = '#00f';
       count.innerHTML = ` ${textareas[i].value.replace(/\s/g, '').length}文字`;
-      countDisplaySpaces[i].appendChild(count);
+      countDisplay.appendChild(count);
     }
   }
 };
