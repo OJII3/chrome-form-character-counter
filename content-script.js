e181e493d00ce114e428c89cbea34e00ef2252a4
OJII3 <kawaisounayuriko@gmail.com>
2022-05-18 09:55:49 +0900

    Update fundtion countWord, function start time

--- content-script.js
+++ content-script.js
@@ -1,23 +1,21 @@
 const countWords = () => {
   const textareas = document.querySelectorAll('.geS5n textarea.KHxj8b, .geS5n input.whsOnd');
-  const countDisplaySpaces = document.querySelectorAll('.geS5n .G4EHhc');
   let counts = document.querySelectorAll('.geS5n .word-counter');
   if (counts[0]) {
     for (let i = 0; i < textareas.length; i++) {
-      counts[i].innerHTML = `${textareas[i].value.replace(/\s/g,'').length} words`;
-      counts[i].innerHTML = counts[i].innerHTML === '0 words' ? '0 word' : counts[i].innerHTML;
+      counts[i].innerHTML = ` ${textareas[i].value.replace(/\s/g, '').length}文字`;
     }
   } else {
     for (let i = 0; i < textareas.length; i++) {
+      const countDisplaySpaces = document.querySelectorAll('.geS5n .G4EHhc');
       const count = document.createElement('span');
       count.className = 'word-counter';
       count.style.color = '#00f';
-      count.innerHTML = `${textareas[i].value.replace(/\s/g,'').length} words`;
-      count.innerHTML = count.innerHTML === '0 words' ? '0 word' : count.innerHTML;
+      count.innerHTML = ` ${textareas[i].value.replace(/\s/g, '').length}文字`;
       countDisplaySpaces[i].appendChild(count);
     }
   }
 };
 
-window.onload = countWords();
+window.addEventListener('load', () => countWords(), false);
 window.addEventListener('input', () => countWords());
